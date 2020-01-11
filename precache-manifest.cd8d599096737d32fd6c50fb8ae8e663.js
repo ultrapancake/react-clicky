1d1f198f54adb6b68a4a01872c824d9f2b5a28f0
Glen <ultrapancake@gmail.com>
2020-01-11 12:55:48 -0700

    Updates

---
+++ precache-manifest.cd8d599096737d32fd6c50fb8ae8e663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4ead4ad607c27812c54279a8909746",
+    "url": "/react-clicky/index.html"
+  },
+  {
+    "revision": "cba12913dc16364b32cd",
+    "url": "/react-clicky/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "54c88ff8f89ce478eabd",
+    "url": "/react-clicky/static/js/2.5968a5c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clicky/static/js/2.5968a5c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cba12913dc16364b32cd",
+    "url": "/react-clicky/static/js/main.d944ca9a.chunk.js"
+  },
+  {
+    "revision": "c7d90f3806589510eae5",
+    "url": "/react-clicky/static/js/runtime-main.8acab4b3.js"
+  }
+]);
