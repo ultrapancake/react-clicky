fb9a23eb930a688c12f2f0cdbd4c74fbc2d30fc6
Glen <ultrapancake@gmail.com>
2020-01-11 12:56:13 -0700

    published app through gh-pages

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 import React from "react";
-import boxing from "./boxing.json"
+import boxing from "./boxing.json";
 import "./App.css";
 
-class App extends React.Component{
-  state ={
+class App extends React.Component {
+  state = {
     topScore: 0,
     score: 0,
     boxing
-  }
+  };
 }
 
-render(){
-  // enter html rendered content here
-  let something = "not a string"
-  return(something);
-}
+// render(){
+//   // enter html rendered content here
+//   let something = "not a string"
+//   return(something);
+// }
 
 export default App;
